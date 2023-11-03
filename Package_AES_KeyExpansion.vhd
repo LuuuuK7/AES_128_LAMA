084589a3cfad87334d5fd0ab16e81a3b4205987c
LuuuuK7 <145787348+LuuuuK7@users.noreply.github.com>
2023-11-03 11:26:35 +0100

    Add files via upload

---
+++ Package_AES_KeyExpansion.vhd
@@ -0,0 +1,29 @@
+--	  ________  ________  ________  ___  __    ________  ________  _______           ________  _______   ________      
+--	 |\   __  \|\   __  \|\   ____\|\  \|\  \ |\   __  \|\   ____\|\  ___ \         |\   __  \|\  ___ \ |\   ____\     
+--	 \ \  \|\  \ \  \|\  \ \  \___|\ \  \/  /|\ \  \|\  \ \  \___|\ \   __/|        \ \  \|\  \ \   __/|\ \  \___|_    
+--	  \ \   ____\ \   __  \ \  \    \ \   ___  \ \   __  \ \  \  __\ \  \_|/__       \ \   __  \ \  \_|/_\ \_____  \   
+--	   \ \  \___|\ \  \ \  \ \  \____\ \  \\ \  \ \  \ \  \ \  \|\  \ \  \_|\ \       \ \  \ \  \ \  \_|\ \|____|\  \  
+--	    \ \__\    \ \__\ \__\ \_______\ \__\\ \__\ \__\ \__\ \_______\ \_______\       \ \__\ \__\ \_______\____\_\  \ 
+--	     \|__|     \|__|\|__|\|_______|\|__| \|__|\|__|\|__|\|_______|\|_______|        \|__|\|__|\|_______|\_________\
+--	                                                                                                      \|_________|
+-- 	Package pour le binôme K : Génération des sous clés
+library ieee;
+use ieee.std_logic_1164.all;
+
+package my_package_2 is
+
+	subtype Word_type is std_logic_vector (31 downto 0);						-- 32 bits
+	
+	function RotWord (a : Word_type) return Word_type;
+	
+end my_package_2;
+
+package body my_package_2 is
+
+-- Fonction RotWord :
+function RotWord (a : Word_type) return Word_type is	
+	begin
+	return a(23 downto 0)&a(31 downto 24);
+end function RotWord ;
+
+end my_package_2;
